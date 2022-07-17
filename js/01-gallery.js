8c9747d6abafe8c0ca06ece56ef01d6485764bf2
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-07-17 17:31:15 +0300

    render_markUp

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`);
+const markUp = createImgMarkUp(galleryItems);
+
+
+gallery.addEventListener(`click`, onGalClick);
+gallery.insertAdjacentHTML(`beforeend`, markUp);
+
+function createImgMarkUp(galleryItems) {
+    const ImgNarkUp = galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+    }).join("");
+    return ImgNarkUp;
+}
+
+// console.log(ImgNarkUp);
+
+function onGalClick(evt) {
+    evt.preventDefault();
+    const isImg = evt.target.classList.contains('gallery__image');
+    if (!isImg) {
+        return
+    };
+    // baza//
+    // import * as basicLightbox from 'basiclightbox'
+    const currentImg = evt.target.dataset.source;
+
+    // console.log(currentImg);
+
+    const instance = basicLightbox.create(`<img src="${currentImg}">`);
+    // console.log(instance);
+
+    instance.show();
+
+}
+
+// console.log(galleryItems);
